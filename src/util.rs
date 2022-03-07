92e678a8931c714207ad560ee5009bd63623237b
AngererSebastian <angerer.sebastian.merlin@gmail.com>
2022-03-07 07:28:43 +0100

    document util functions

--- src/util.rs
+++ src/util.rs
@@ -1,14 +1,18 @@
 use bevy::prelude::*;
 
-pub fn mirror_vector(vec: Vec2, norm: Vec2) -> Vec2 {
+/// mirrors an vector along an side
+pub fn mirror_vector(vec: Vec2, side: Vec2) -> Vec2 {
     // https://stackoverflow.com/questions/56274674/how-to-mirror-a-vector-along-a-surface
-    vec - 2.0 * vec.dot(norm) * norm
+    vec - 2.0 * vec.dot(side) * side
 }
 
+/// creates an vector with the given magnitude and
+/// the given angle on the x-axis
 pub fn vector_at_angle(magnitude: f32, angle: f32) -> Vec2 {
     magnitude * Vec2::new(angle.cos(), angle.sin())
 }
 
+/// converts Screen coordinates to world coordinates
 pub fn screen_to_world(
     screen: Vec2,
     window: &Window,
